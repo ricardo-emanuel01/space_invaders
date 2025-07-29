5dfad68b359ee876cfbc3a4b102980c83ff7f36c
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-07-29 17:36:41 -0300

    :construction: Add logic that enable shooting
    - Implement bullet as an entity;
    - Implement shot function which generates a shot of one not alive shot
      on the Entities Array;
    - Update function verify if the entity can shot;
    - The game stops when enter on the confirmation of exit.

--- src/main.c
+++ src/main.c
@@ -15,13 +15,18 @@
 # define ENEMIES_GAP_X 10
 # define ENEMIES_GAP_Y 15
 # define ENEMIES_WIDTH 40
+# define ENEMIES_HEIGHT ENEMIES_WIDTH
 # define ENEMIES_OFFSET_Y (ENEMIES_WIDTH*3)
 # define N_ENEMIES_ROWS 6
 # define ENEMIES_PER_ROW 12
 # define N_ENEMIES (N_ENEMIES_ROWS*ENEMIES_PER_ROW)
 # define EXIT_BANNER_HEIGHT 200
 # define EXIT_MESSAGE_FONT_SIZE 75
-# define BULLET_SPEED 75 // pixels/frame
+# define BULLET_SPEED 20 // pixels/frame
+# define N_BULLETS 100
+# define BULLET_WIDTH 20
+# define BULLET_HEIGHT 40
+# define ENTITIES_ARRAY_SIZE (2+N_ENEMIES+N_BULLETS)
 
 
 const char exitMessage[] = "Are you sure you want to exit game? [Y/N]";
@@ -42,12 +47,14 @@ typedef struct Entity {
     Color color;
     bool alive;
     float delayToShoot;
+    bool canShot;
+    double lastShotTime;
 } Entity;
 
 Entity *buildEntities(Color colors[], float delayToShoot[]) {
     EntityType entityType = SHIP;
 
-    Entity *entities = (Entity *)malloc((N_ENEMIES + 2) * sizeof(Entity));
+    Entity *entities = (Entity *)malloc((ENTITIES_ARRAY_SIZE) * sizeof(Entity));
     // Setup player ship
     entities[0].type = entityType;
     entities[0].dimensions.x = SHIP_WIDTH;
@@ -59,6 +66,8 @@ Entity *buildEntities(Color colors[], float delayToShoot[]) {
     entities[0].color = colors[0];
     entities[0].alive = true;
     entities[0].delayToShoot = delayToShoot[entityType];
+    entities[0].canShot = true;
+    entities[0].lastShotTime = GetTime();
 
     int enemiesXOffSet = (SCREEN_WIDTH - ENEMIES_PER_ROW*(ENEMIES_WIDTH + ENEMIES_GAP_X))/2;
     for (int i = 0; i < N_ENEMIES; ++i) {
@@ -73,6 +82,8 @@ Entity *buildEntities(Color colors[], float delayToShoot[]) {
         entities[i+2].color = colors[entityType];
         entities[i+2].alive = true;
         entities[i+2].delayToShoot = delayToShoot[entityType];
+        entities[i+2].canShot = true;
+        entities[i+2].lastShotTime = GetTime();
     }
 
     // Setup Enemy ship
@@ -85,13 +96,23 @@ Entity *buildEntities(Color colors[], float delayToShoot[]) {
     entities[1].velocity.y = 0.0f;
     entities[1].color = colors[entityType];
     entities[1].alive = false;
-    entities[1].delayToShoot = delayToShoot[entityType];
+    entities[1].delayToShoot = delayToShoot[entityType++];
+    entities[1].canShot = true;
+    entities[1].lastShotTime = GetTime();
+
+    for (int i = N_ENEMIES + 2; i < ENTITIES_ARRAY_SIZE; ++i) {
+        entities[i].type = entityType;
+        entities[i].dimensions.x = BULLET_WIDTH;
+        entities[i].dimensions.y = BULLET_HEIGHT;
+        entities[i].alive = false;
+        entities[i].color = RAYWHITE;
+    }
 
     return entities;
 }
 
 void drawEntites(Entity *entities) {
-    for (int i = 0; i < N_ENEMIES+2; ++i) {
+    for (int i = 0; i < ENTITIES_ARRAY_SIZE; ++i) {
         if (entities[i].alive)
             DrawRectangle(
                 entities[i].position.x,
@@ -121,7 +142,7 @@ void drawExitMessage() {
 }
 
 void debugEntities(Entity *entities) {
-    for (int i = 0; i < N_ENEMIES+2; ++i) {
+    for (int i = 0; i < ENTITIES_ARRAY_SIZE; ++i) {
         printf("Entity at index: %d\n", i);
         printf("\tType: %d\n", entities[i].type);
         printf("\tWidth: %lf\n", entities[i].dimensions.x);
@@ -133,12 +154,42 @@ void debugEntities(Entity *entities) {
 }
 
 void updateEntities(Entity *entities) {
-    for (int i = 0; i < N_ENEMIES + 2; ++i) {
+    for (int i = 0; i < ENTITIES_ARRAY_SIZE; ++i) {
         if (entities[i].alive) {
             entities[i].position.x += entities[i].velocity.x;
-            entities[i].velocity.x = 0.0f;
             entities[i].position.y += entities[i].velocity.y;
-            entities[i].velocity.y = 0.0f;
+            if (entities[i].type == SHIP) {
+                entities[i].velocity.x = 0.0f;
+                entities[i].velocity.y = 0.0f;
+            }
+            if (entities[i].type != BULLET) {
+                double currentTime = GetTime();
+                if (currentTime - entities[i].lastShotTime > entities[i].delayToShoot) {
+                    entities[i].canShot = true;
+                }
+            }
+            if (entities[i].position.y < 0.0f) entities[i].alive = false;
+        }
+    }
+}
+
+void shot(Entity *entities, int shooterIdx) {
+    for (int i = N_ENEMIES + 2; i < ENTITIES_ARRAY_SIZE; ++i) {
+        if (!entities[i].alive) {
+            entities[shooterIdx].canShot = false;
+            entities[shooterIdx].lastShotTime = GetTime();
+
+            entities[i].position.x = entities[shooterIdx].position.x + 0.5*entities[shooterIdx].dimensions.x;
+            entities[i].alive = true;
+
+            if (entities[shooterIdx].type == SHIP) {
+                entities[i].velocity.y = -BULLET_SPEED;
+                entities[i].position.y = entities[shooterIdx].position.y;
+            } else {
+                entities[i].velocity.y = BULLET_SPEED;
+                entities[i].position.y = entities[shooterIdx].position.y + entities[shooterIdx].dimensions.y;
+            }
+            break;
         }
     }
 }
@@ -160,7 +211,6 @@ int main(void) {
     Entity *entities = buildEntities(colors, delayToShoot);
     bool exitWindowRequested = false;
     bool exitWindow = false;
-    Font defaultFont = GetFontDefault();
 
     while (!exitWindow) {
         if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
@@ -168,12 +218,14 @@ int main(void) {
         if (exitWindowRequested) {
             if (IsKeyPressed(KEY_Y)) exitWindow = true;
             else if (IsKeyPressed(KEY_N)) exitWindowRequested = false;
+        } else {
+            // Input processing
+            if (IsKeyDown(KEY_LEFT)) entities[0].velocity.x = -10;
+            if (IsKeyDown(KEY_RIGHT)) entities[0].velocity.x = 10;
+            if (IsKeyDown(KEY_SPACE) && entities[0].canShot) shot(entities, 0);
+    
+            updateEntities(entities);
         }
-
-        if (IsKeyDown(KEY_LEFT)) entities[0].velocity.x = -10;
-        if (IsKeyDown(KEY_RIGHT)) entities[0].velocity.x = 10;
-
-        updateEntities(entities);
 
         // Without 'BeginDrawing()' and 'EndDrawing()' the mainloop doesn't work
         BeginDrawing();
